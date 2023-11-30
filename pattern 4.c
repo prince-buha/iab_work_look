bfdc9e97d63e8ec6a44ee30e368258a3e0400a5a
prince-buha <150029430+prince-buha@users.noreply.github.com>
2023-11-30 15:21:45 +0530

    Update pattern 4.c

--- pattern 4.c
+++ pattern 4.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int main() {
+    int i,j;
     int rows = 5;
     int count = 1;
     char ch = 'A';
-    int i,j;
+
 
     for ( i = 0; i < rows; i++) {
         for ( j = 0; j < 5; j++) {
